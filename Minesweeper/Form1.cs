611f55bd9e7d92cab6b9277eb7b3b5a9dc293cd2
Maxim Cherepanov <9045857.mc@gmail.com>
2019-03-22 08:03:38 +0700

    переименовал форму

--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Minesweeper
 {
-    public partial class Form1 : Form
+    public partial class MinesweeperForm : Form
     {
-        public Form1()
+        public MinesweeperForm()
         {
             InitializeComponent();
         }
