91af790dbe4268c8360ed9686093b2cebb0b7f5f
Maxim Cherepanov <9045857.mc@gmail.com>
2019-05-08 15:10:19 +0700

    ВАЖНО переделаываем решенин на три проекта.

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -7,31 +7,39 @@ using Minesweeper.Gui;
 using Minesweeper.Logic;
 using Minesweeper.TextUi;
 
+
 namespace Minesweeper
 {
     static class Program
     {
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
             //   Application.EnableVisualStyles();// нужно перед созданием первой формы
             //   Application.SetCompatibleTextRenderingDefault(false);// нужно перед созданием первой формы
             //   Application.Run(new MainForm());
 
+            
+
+
             System.Console.WriteLine("Игра Сапер. Текстовая версия.");
-            System.Console.WriteLine();
+                System.Console.WriteLine();
 
-            GameParameters gameParameters = new GameParameters(10, 15, 10, false);
-            GameLogic gameLogic = new GameLogic(gameParameters);
-            GameText gameText = new GameText(gameParameters, gameLogic);
+                GameParameters gameParameters = new GameParameters(10, 15, 10, false);
+                GameLogic gameLogic = new GameLogic(gameParameters);
+                GameText gameText = new GameText(gameParameters, gameLogic);
 
-            gameText.Do("");
+                gameText.Do("");
+            Console.WriteLine();
 
             Console.ReadLine();
-
+        
         }
     }
 }
